8a4a441f004b9161c081935a24ae728a83f44ef2
Tejas Rawal <trawal90@gmail.com>
2025-02-26 10:33:12 -0500

    add logger config. update imports.

--- src/server/config/axios.ts
+++ src/server/config/axios.ts
@@ -4,7 +4,5 @@ import { environment } from './environment';
 const { dashApi } = environment;
 
 // Create a custom instance of axios
-const axiosInstance = axios.create({ baseURL: dashApi.baseUrl });
+export const axiosInstance = axios.create({ baseURL: dashApi.baseUrl });
 axiosInstance.defaults.headers.common.Authorization = dashApi.apiKey;
-
-export default axiosInstance;
