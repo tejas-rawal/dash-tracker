19c5f11ccd178479d654a136e9d506e8fc7b7db0
Tejas Rawal <trawal90@gmail.com>
2025-03-03 17:41:06 -0500

    update model interfaces to be classes.

--- src/server/api/models/BusRoute.ts
+++ src/server/api/models/BusRoute.ts
@@ -1,24 +1,55 @@
 import type { BusStop } from './BusStop';
+import type { RouteDirection } from './RouteDirection';
 
-enum RouteType {
+export enum RouteType {
     Tram = "0",
     Subway = "1",
     Rail = "2",
     Bus = "3",
 }
 
-interface RouteDirection {
-    id: string,
-    title: string,
-    stops: BusStop[],
-    headSigns: string[]
-}
-
-export interface BusRoute {
+export class BusRoute {
     id: string;
     longName: string;
     shortName: string;
     name: string;
     type: RouteType;
     directions: RouteDirection[];
-}
+
+    constructor(data: {
+      id: string;
+      longName: string;
+      shortName: string;
+      name: string;
+      type: RouteType;
+      directions: RouteDirection[];
+    }) {
+      this.id = data.id;
+      this.longName = data.longName;
+      this.shortName = data.shortName;
+      this.name = data.name;
+      this.type = data.type;
+      this.directions = data.directions;
+    }
+
+    // Add methods
+    getAllStops(): BusStop[] {
+      const allStops: BusStop[] = [];
+      const stopIds = new Set<string>();
+
+      for (const direction of this.directions) {
+        for (const stop of direction.stops) {
+          if (!stopIds.has(stop.id)) {
+            stopIds.add(stop.id);
+            allStops.push(stop);
+          }
+        }
+      }
+
+      return allStops;
+    }
+
+    getDirectionById(directionId: string): RouteDirection | undefined {
+      return this.directions.find(dir => dir.id === directionId);
+    }
+  }
